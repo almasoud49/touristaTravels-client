1688ff6ca77e5bae00cd83ad139b794346faed71
Abdullah Al Masoud <almasoud49@yahoo.com>
2024-05-01 07:49:20 +0600

    [DONE] pagination  section

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,36 +1,48 @@
 import { useEffect, useState } from "react";
 import { axiosSecure } from "../../hooks/useAxios";
 import ServiceCard from "./ServiceCard";
+import Pagination from "../shared/Pagination/Pagination";
 
 const Services = () => {
-    const [services, setServices] = useState([]);
-	const [count, setCount] = useState(0);
-	const [size, setSize] = useState(6);
-	const [page,setPage] = useState(0)
-	const pages = Math.ceil(count / size);
+  const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(false)
+  const [count, setCount] = useState(0);
+  const [size, setSize] = useState(6);
+  const [page, setPage] = useState(0);
+  const pages = Math.ceil(count / size);
 
-	const paginationInfo = {
-		size,
-		setSize,
-		page,
-		setPage,
-		pages
-	}
+  const paginationInfo = {
+    size,
+    setSize,
+    page,
+    setPage,
+    pages,
+  };
 
-    useEffect(()=>{
-        const url = `/services?size=${size}&page=${page}`
-        axiosSecure.get(url)
-        .then(res => setServices(res.data))
-    },[page,size])
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 ml-10">
-            {
-                services.map((service)=>(
-                    <ServiceCard key={service._id} service={service}></ServiceCard>
-                ))
-            }
-        </div>
-    );
+  useEffect(() => {
+    setLoading(true)
+    const url = `/services?size=${size}&page=${page}`;
+        axiosSecure.get(url).then((res) => {
+        setServices(res.data.result)
+        setCount(res.data.count)
+        setLoading(false)
+    
+    });
+  }, [page, size]);
+  return (
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 ml-10">
+        
+        {
+            services?.map((service)=>(
+               <ServiceCard key={service._id} service={service}></ServiceCard> 
+            ))
+        }
+
+      </div>
+      <Pagination paginationInfo={paginationInfo}/>
+    </>
+  );
 };
 
 export default Services;
