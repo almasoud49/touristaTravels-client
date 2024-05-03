381648c1f3b813ce5fdc11c92dfe5d4cd8fe2833
Abdullah Al Masoud <almasoud49@yahoo.com>
2024-05-03 13:12:03 +0600

    [DONE] see all service section under 3 service items

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { axiosSecure } from "../../../hooks/useAxios";
 import ServiceCard from "../../services/ServiceCard";
+import { Link } from "react-router-dom";
 const Home = () => {
   const [services, setServices] = useState([]);
 
@@ -11,11 +12,23 @@ const Home = () => {
   }, [url]);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 ml-10">
-      {services.map((service) => (
-        <ServiceCard key={service._id} service={service}></ServiceCard>
-      ))}
-    </div>
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 ml-10">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service}></ServiceCard>
+        ))}
+      </div>
+      <div className="flex flex-col w-full">
+        <div className="divider divider-primary mt-10">
+          <Link
+            to="/services"
+            className='className="mt-10 block btn btn-primary  px-12 py-3 text-sm font-bold uppercase tracking-wider text-white transition focus:outline-none'
+          >
+            See All
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
